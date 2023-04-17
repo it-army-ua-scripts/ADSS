3096624504e08ae5204e4bdc60c8a7f8193c1f88
panchvitm <130738822+panchvitm@users.noreply.github.com>
2023-04-17 09:06:57 +0300

    Add function for defining os version

--- install-ufw.sh
+++ install-ufw.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+get_distribution() {
+	lsb_dist=""
+
+	if [ -r /etc/os-release ]; then
+		lsb_dist="$(. /etc/os-release && echo "$ID")"
+	fi
+
+	echo "$lsb_dist"
+}
+
+lsb_dist=$( get_distribution )
+lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+
 install_ufw(){
     echo -e "${GREEN}Встановлюємо UFW фаєрвол${NC}"
 
