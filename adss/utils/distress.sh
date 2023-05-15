6eef4a7646565d319f62bbc5417b2383a80f5522
OleksandrBlack <aleksandr.chornuy@gmail.com>
2023-05-15 21:02:33 +0300

    more_arch

--- adss/utils/distress.sh
+++ adss/utils/distress.sh
@@ -9,8 +9,29 @@ install_distress() {
     cd $WORKING_DIR
     echo -e "${GREEN}Встановлюємо Distress${NC}"
 
-    sudo curl -Lo distress_x86_64-unknown-linux-musl https://github.com/Yneth/distress-releases/releases/latest/download/distress_x86_64-unknown-linux-musl
-    sudo chmod +x distress_x86_64-unknown-linux-musl
+    OSARCH=$(uname -m)
+
+    case "$OSARCH" in
+      aarch64*)
+        sudo curl -Lo distress_aarch64-unknown-linux-musl https://github.com/Yneth/distress-releases/releases/latest/download/distress_aarch64-unknown-linux-musl
+        sudo chmod +x distress_aarch64-unknown-linux-musl
+      ;;
+	  
+      x86_64*)
+        sudo curl -Lo distress_x86_64-unknown-linux-musl https://github.com/Yneth/distress-releases/releases/latest/download/distress_x86_64-unknown-linux-musl
+        sudo chmod +x distress_x86_64-unknown-linux-musl
+      ;;
+	  
+      i386* | i686*)
+        sudo curl -Lo distress_i686-unknown-linux-musl https://github.com/Yneth/distress-releases/releases/latest/download/distress_i686-unknown-linux-musl
+        sudo chmod +x distress_i686-unknown-linux-musl
+      ;;
+	  
+      *)
+        echo "Неможливо визначити розрядность операційної системи";
+        exit 1
+      ;;
+    esac
 
     echo -e "${GREEN}Distress успішно встановлено${NC}"
 }
