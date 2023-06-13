b07986281522e42de0c07244eef8ae9a65dc0889
termos007 <leerndeg@gmail.com>
2023-06-13 16:30:59 +0300

    auto update script

--- utils/updater.sh
+++ utils/updater.sh
@@ -2,22 +2,35 @@
 
 check_updates() {
   timestamp=$(date +%s)
-  if [[ -z "$ADSS_DEPLOYMENT_VERSION" ]]; then
-    export ADSS_DEPLOYMENT_VERSION=$timestamp
+  source "/etc/environment"
+  if [ -z "$ADSS_DEPLOYMENT_VERSION" ]; then
+      write_version $timestamp
+      prepare_for_update
+  else
+      diff=$((timestamp - ADSS_DEPLOYMENT_VERSION))
+      one_hour=3600
+      if [[ $diff -gt $one_hour  ]];then
+        prepare_for_update
+      fi
   fi
-  diff=$((timestamp - ADSS_DEPLOYMENT_VERSION))
-  one_hour=3600
-  need_to_update=$(echo "$diff > $one_hour" | bc -l)
-  if [[ $need_to_update -eq 1  ]];then
-    echo -e "${GREEN}Перевіряємо наявленість оновлень${NC}"
-    current_version=$(<"$SCRIPT_DIR"/version.txt)
-    current_version=${current_version//[$'\t\r\n']}
-    remote_version=$(curl -s URL_файлу)
-    if [[ $current_version -gt $remote_version  ]];then
-        update_adss
-        export ADSS_DEPLOYMENT_VERSION=$(date +%s)
-    fi
+}
+prepare_for_update() {
+  echo -e "${GREEN}Перевіряємо наявленість оновлень${NC}"
+  current_version=$(<"$SCRIPT_DIR"/version.txt)
+  current_version=${current_version//[$'\t\r\n']}
+  remote_version=$(curl -s URL_файлу)
+  need_update=$(echo "$current_version < $remote_version" | bc -l)
+  if [[ $need_update -eq 1  ]];then
+      update_adss
+      write_version $(date +%s)
   fi
+  sleep 2
+}
+write_version() {
+  env_file="/etc/environment"
+  sudo sed -i '/ADSS_DEPLOYMENT_VERSION/d' $env_file
+  echo "ADSS_DEPLOYMENT_VERSION=\"$1\"" | sudo tee -a $env_file > /dev/null
+  source $env_file
 }
 
 update_adss() {
