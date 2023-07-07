a852899fe62a0cacfa2878c9cd9b720999e38506
termos007 <leerndeg@gmail.com>
2023-07-07 19:06:46 +0300

    big fixing

--- utils/docker.sh
+++ utils/docker.sh
@@ -7,13 +7,16 @@ install_docker() {
           debian)
               adss_dialog "Встановлюємо докер"
               install() {
-                 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
-                  sudo chmod a+r /etc/apt/keyrings/docker.gpg
-                  echo \
+                 sudo apt-get update -y
+                 sudo apt-get install ca-certificates curl gnupg -y
+                 sudo install -m 0755 -d /etc/apt/keyrings
+                 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                 sudo chmod a+r /etc/apt/keyrings/docker.gpg
+                 echo \
                     "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
                     "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-                    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                  sudo apt-get update -y
+
+                  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null 2>&1
                   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
                   sudo systemctl start docker
               }
