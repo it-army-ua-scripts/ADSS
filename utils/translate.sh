a5f21b9ccd28a2a0c86efa38d1d72e8af67dac4c
termos007 <leerndeg@gmail.com>
2023-07-14 11:57:59 +0300

    refactoring

--- utils/translate.sh
+++ utils/translate.sh
@@ -2,20 +2,22 @@
 
 load_translation_file() {
   local PATH_TO_LOCALIZATION="$SCRIPT_DIR/i18n"
+  lang_param_found=false
   for arg in "$@"; do
-    if [[ $arg == "--lang" ]]; then
-      case "$arg" in
-          en)
-            source "$PATH_TO_LOCALIZATION/en.sh"
-            ;;
-          *)
-            source "$PATH_TO_LOCALIZATION/ua.sh"
-            ;;
-        esac
-    else
-      source "$PATH_TO_LOCALIZATION/ua.sh"
-    fi
+      if [[ "$arg" == "--lang" ]]; then
+          lang_param_found=true
+      elif [[ "$arg" == "en" && "$lang_param_found" == true ]]; then
+          source "$PATH_TO_LOCALIZATION/en.sh"
+          break
+      elif [[ "$arg" == "ua" && "$lang_param_found" == true ]]; then
+          source "$PATH_TO_LOCALIZATION/ua.sh"
+          break
+      fi
   done
+
+  if [[ "$lang_param_found" == false ]]; then
+    source "$PATH_TO_LOCALIZATION/ua.sh"
+  fi
 }
 
 trans() {
