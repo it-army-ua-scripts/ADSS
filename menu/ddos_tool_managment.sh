9b166b136dd63a6bc0461bc48a58bcbe9f4f4afb
OleksandrBlack <aleksandr.chornuy@gmail.com>
2023-10-24 23:39:28 +0300

    better

--- menu/ddos_tool_managment.sh
+++ menu/ddos_tool_managment.sh
@@ -57,14 +57,12 @@ get_ddoss_status() {
 	  #Fix Parrot
       #journalctl -n 20 -u "$service.service" --no-pager
 
-      #Fix Ubuntu 12-18
+      #Fix Ubuntu < 19
       lsb_version="$(. /etc/os-release && echo "$VERSION_ID")"
       lsb_id="$(. /etc/os-release && echo "$ID")"
+	  
       if [[ "$lsb_id" == "ubuntu" ]] &&
-         [[ "$lsb_version" == 12* ]] ||
-         [[ "$lsb_version" == 14* ]] ||
-         [[ "$lsb_version" == 16* ]] ||
-         [[ "$lsb_version" == 18* ]]; then
+         [[ "$lsb_version" < 19* ]]; then
         journalctl -n 20 -u "$service.service" --no-pager
       else
         tail --lines=20 /var/log/adss.log
