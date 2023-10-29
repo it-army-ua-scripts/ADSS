01d456071d1b9c2214e06642b5b6ef36c7fcb145
OleksandrBlack <aleksandr.chornuy@gmail.com>
2023-10-29 17:29:36 +0200

    distress flood i18n

--- i18n/en.sh
+++ i18n/en.sh
@@ -108,6 +108,8 @@ declare -A localization=(
 
   ["Відсоткове співвідношення використання власної IP адреси (0-100): "]="Percentage of personal IP address usage (0-100): "
   ["Увімкнути UDP flood (1 | 0): "]="Enable UDP flood (1 | 0): "
+  ["Розмір UDP пакунку: "]="UDP packet size: "
+  ["Кількість пакетів: "]="Number of packets: "
   ["Кількість підключень Tor (0-100): "]="Number of Tor connections (0-100): "
   ["Кількість створювачів завдань (4096): "]="Number of task creators (4096): "
 
