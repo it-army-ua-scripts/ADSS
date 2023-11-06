3679e088c2b6cead8c3949c3745ed73e34d5b410
OleksandrBlack <aleksandr.chornuy@gmail.com>
2023-11-06 19:44:23 +0200

    Centos already exists in another branch
    https://github.com/it-army-ua-scripts/ADSS/blob/78f1914f7ee17ef5ac011263abade5b613ce465f/utils/ufw.sh#L18

--- utils/ufw.sh
+++ utils/ufw.sh
@@ -3,16 +3,11 @@
 source "${SCRIPT_DIR}/utils/definitions.sh"
 install_ufw() {
   case $(get_distribution) in
-  fedora | rocky | ol | centos)
+  fedora | rocky | ol)
     adss_dialog "$(trans "Встановлюємо UFW фаєрвол")"
     install() {
       if [ $lsb_dist == "ol" ]; then 
         sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-      else
-        return 0
-      fi
-      if [ $lsb_dist == "centos" ]; then
-        udo dnf install epel-release
       else
         return 0
       fi
