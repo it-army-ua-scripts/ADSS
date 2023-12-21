673f4837d4dc7eab3e9e0f0878b171f6a7178bc6
Andrii <leerndeg@gmail.com>
2023-12-21 22:36:29 +0200

    refactor

--- utils/mhddos.sh
+++ utils/mhddos.sh
@@ -146,7 +146,7 @@ regenerate_mhddos_service_file() {
   lines=$(sed -n "/\[mhddos\]/,/\[\/mhddos\]/p" "${SCRIPT_DIR}"/services/EnvironmentFile)
 
   start="ExecStart=$SCRIPT_DIR/bin/mhddos_proxy_linux"
-  vpn=false
+
   while read -r line
   do
     key=$(echo "$line"  | cut -d '=' -f1)
