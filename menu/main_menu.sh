6ae29b8afb323f1daac49308be675a80a199d78c
Andrii <leerndeg@gmail.com>
2024-01-18 21:20:21 +0200

    Revert "developing"
    This reverts commit d3b96ed67dbceddf4f53e38d35d993f76f0800be.

--- menu/main_menu.sh
+++ menu/main_menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 main_menu() {
-  menu_items=("$(trans "Розширення портів")" "$(trans "Налаштування безпеки")" "DDOS" "$(trans "Статус атаки")")
+  menu_items=("$(trans "Розширення портів")" "$(trans "Налаштування безпеки")" "DDOS")
   res=$(display_menu "$(trans "Головне меню")" "${menu_items[@]}")
 
   case "$res" in
@@ -14,10 +14,6 @@ main_menu() {
       ;;
     "DDOS")
       ddos
-      ;;
-    "$(trans "Статус атаки")")
-      get_ddoss_status
-      main_menu
       ;;
   esac
 }
