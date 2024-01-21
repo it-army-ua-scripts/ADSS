8f4ecac05844cb28542664de9f0164f79f88d7c1
OleksandrBlack <aleksandr.chornuy@gmail.com>
2024-01-21 20:38:35 +0200

    1.2.0
    ddoss_statu in main_menu

--- menu/main_menu.sh
+++ menu/main_menu.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 main_menu() {
-  menu_items=("$(trans "Розширення портів")" "$(trans "Налаштування безпеки")" "DDOS" "$(trans "Статус атаки")")
+  menu_items=("$(trans "Статус атаки")" "$(trans "Розширення портів")" "DDOS" "$(trans "Налаштування безпеки")")
   res=$(display_menu "$(trans "Головне меню")" "${menu_items[@]}")
 
   case "$res" in
+    "$(trans "Статус атаки")")
+      get_ddoss_status
+      main_menu
+      ;;
     "$(trans "Розширення портів")")
       extend_ports
       main_menu
       ;;
-    "$(trans "Налаштування безпеки")")
-      security_settings
-      ;;
     "DDOS")
       ddos
       ;;
-    "$(trans "Статус атаки")")
-      get_ddoss_status
-      main_menu
+    "$(trans "Налаштування безпеки")")
+      security_settings
       ;;
   esac
 }
