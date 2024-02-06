ab6d0dd211d618993db0030ded605c161881d581
Andrii <leerndeg@gmail.com>
2024-02-06 22:16:54 +0200

    developing

--- utils/datapatch.sh
+++ utils/datapatch.sh
@@ -75,7 +75,7 @@ apply_patch() {
   # for 1.2.1
   udpPackageSize=$(sed -n '/\[distress\]/,/\[\/distress\]/ s/udp-packet-size=\([0-9]\+\)/\1/p' "$envFile")
   if [[ $udpPackageSize -gt 1420 ]]; then
-    sed -i '/\[distress\]/,/\[\/distress\]/ s/udp-packet-size=[0-9]\+/udp-packet-size=1420/g' "$envFile"
+    sed -i '/\[distress\]/,/\[\/distress\]/ s/udp-packet-size=[0-9]\+/udp-packet-size=1252/g' "$envFile"
       regenerate_distress_service_file
   fi
   # for 1.2.1
