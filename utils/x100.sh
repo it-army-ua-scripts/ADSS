7887a83a46c13069714e219f9b5bc6da1d21e0de
Andrii <leerndeg@gmail.com>
2024-05-09 20:36:58 +0300

    develop

--- utils/x100.sh
+++ utils/x100.sh
@@ -45,22 +45,9 @@ docker_installed() {
 install_docker() {
   if [ -r /etc/os-release ]; then
     clear
-    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-    # Add Docker's official GPG key:
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-    # Add the repository to Apt sources:
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt install -y docker.io
     sudo service docker start
+    sudo service docker enable
     sudo groupadd docker
     sudo usermod -aG docker $USER
   else
