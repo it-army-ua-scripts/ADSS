15dc56e2493549df8525ae0fbbcf069f0dfac87b
leernd007 <60017624+leernd007@users.noreply.github.com>
2024-05-10 12:42:49 +0300

    Update ddos_tool_managment.sh

--- menu/ddos_tool_managment.sh
+++ menu/ddos_tool_managment.sh
@@ -69,7 +69,7 @@ get_ddoss_status() {
       fi
 
       echo -e "${ORANGE}$(trans "Нажміть будь яку клавішу щоб продовжити")${NC}"
-
+      sleep 3
       if read -rsn1 -t 0.1; then
         break
       fi
