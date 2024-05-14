7a06aa87afa8a2afab978cb4c2f49c475f7ddb09
leernd007 <60017624+leernd007@users.noreply.github.com>
2024-05-14 12:53:33 +0300

    Update x100.sh

--- utils/x100.sh
+++ utils/x100.sh
@@ -127,12 +127,12 @@ configure_x100() {
 
     sed -i -e "s/itArmyUserId=$(get_x100_variable 'itArmyUserId')/itArmyUserId=$user_id/g" "$configPath"
 
-    read -e -p "$(trans "Initial Distress Scale (0-100): ")" -i "$(get_x100_variable 'initialDistressScale')"  scale
+    read -e -p "$(trans "Initial Distress Scale (10-1000): ")" -i "$(get_x100_variable 'initialDistressScale')"  scale
     if [[ -n "$scale" ]];then
-      while [[ $scale -lt 0 || $scale -gt 100 ]]
+      while [[ $scale -lt 10 || $scale -gt 1000 ]]
       do
         echo "$(trans "Будь ласка введіть правильні значення")"
-        read -e -p "$(trans "Initial Distress Scale (0-100): ")" -i "$(get_x100_variable 'initialDistressScale')" scale
+        read -e -p "$(trans "Initial Distress Scale (10-1000): ")" -i "$(get_x100_variable 'initialDistressScale')" scale
       done
     fi
     sed -i -e "s/initialDistressScale=$(get_x100_variable 'initialDistressScale')/initialDistressScale=$scale/g" "$configPath"
