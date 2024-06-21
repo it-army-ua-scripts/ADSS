a506e08583e901a74fe6dd3d7514873ab7183fcb
Andrii <leerndeg@gmail.com>
2024-06-21 17:12:21 +0300

    db1000n removing

--- menu/autoloading.sh
+++ menu/autoloading.sh
@@ -39,14 +39,14 @@ autoload_configuration() {
     mhddos_auto_enable
     autoload_configuration
     ;;
-  "$(trans "Вимкнути автозапуск DB1000N")")
-    db1000n_auto_disable
-    autoload_configuration
-    ;;
-  "$(trans "Увімкнути автозапуск DB1000N")")
-    db1000n_auto_enable
-    autoload_configuration
-    ;;
+#  "$(trans "Вимкнути автозапуск DB1000N")")
+#    db1000n_auto_disable
+#    autoload_configuration
+#    ;;
+#  "$(trans "Увімкнути автозапуск DB1000N")")
+#    db1000n_auto_enable
+#    autoload_configuration
+#    ;;
   "$(trans "Вимкнути автозапуск DISTRESS")")
     distress_auto_disable
     autoload_configuration
