6bf9cad2924f6d963239c58a5a720cd157b6a363
leernd007 <60017624+leernd007@users.noreply.github.com>
2025-03-03 22:22:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,7 +81,7 @@ if [ -r /etc/os-release ]; then
       elif [ "$PACKAGE_MANAGER" == "emerge" ]; then
           git clone -b gentoo https://github.com/it-army-ua-scripts/ADSS.git "$WORKING_DIR"
       else
-          git clone https://github.com/it-army-ua-scripts/ADSS.git "$WORKING_DIR"
+          git clone -b time_running https://github.com/it-army-ua-scripts/ADSS.git "$WORKING_DIR"
       fi
     fi
 
