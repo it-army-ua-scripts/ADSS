b7e135b22a6086aea36ca44d9f2b61c060c4d6e5
OleksandrBlack <aleksandr.chornuy@gmail.com>
2026-04-04 21:07:24 +0300

    Revert "Позиція"
    This reverts commit 87cfcda875172ce6f36da243d716f60ccf891058.

--- menu/menu_init.sh
+++ menu/menu_init.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
 
-wrap_dialog_text() {
-  local text="$1"
-  local width="${2:-72}"
-  local wrapped=""
-  local line=""
-
-  while IFS= read -r line || [[ -n "$line" ]]; do
-    if [[ -z "$line" ]]; then
-      wrapped+=$'\n'
-      continue
-    fi
-
-    wrapped+="$(printf '%s' "$line" | fold -s -w "$width")"$'\n'
-  done <<< "$text"
-
-  printf '%s' "${wrapped%$'\n'}"
-}
-
 display_menu() {
   title="$1"
   shift
-  local prompt_text
-  if [[ "$1" == "--text" ]]; then
-    prompt_text="$2"
-    shift 2
-  else
-    prompt_text="$(trans "Оберіть опцію:")"
-  fi
   options=("$@")
   local dialog_args=()
   local index
@@ -35,7 +10,7 @@ display_menu() {
     dialog_args+=("${options[index]}" "")
   done
   local selection=$(dialog --ascii-lines --clear --stdout --cancel-label "$(trans "Вихід")" --title "$title" \
-    --menu "$prompt_text" 0 0 0 "${dialog_args[@]}")
+    --menu "$(trans "Оберіть опцію:")" 0 0 0 "${dialog_args[@]}")
 
   if [[ -z "$selection" ]]; then
     clear >$(tty)
